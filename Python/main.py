fbd8d6e293ac892f1c898f859f67f8e973ea2fee
Isaiah Vickers <isaiah.vickers@outlook.com>
2023-09-15 01:22:26 -0400

    Update Py Main Menu Features

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,25 @@
 import os
+import sys
 import time
 
 
 def run():
     run = TaskManager()
     run.clear_system()
-    run.main_menu()
+    
+    # Program functions
+    while True:
+        menu_option = run.main_menu()
+        
+        if menu_option == 'q':
+            break
+        elif menu_option == 'a':
+            run.add_tasks()
+        elif menu_option == 'r':
+            run.remove_tasks()
+        elif menu_option == 's':
+            run.task_view()
+        
 
     # Exit program
     confirm_exit = input("\nPress ENTER or any key to exit...")
@@ -20,18 +34,24 @@ class TaskManager:
         self.task_amount = 0
 
     def main_menu(self):
+        """
+            Main menu feature. 
+            The user must enter a, r, s, or q in order to successfully exit the menu.
+        """
         print("What would you like to do?")
         print("'a' to Add Tasks, 'r' to Remove Tasks, 's' to Show Tasks, or 'q' to Quit")
 
         prompt = input()
         valid_menu_option = self.is_valid_menu_option(prompt)
 
-        while not valid_menu_option or not prompt:
+        while not valid_menu_option or not prompt: # does not exit until valid letter is entered
             print("Invalid Response.\n")
             print("What would you like to do?")
             print("'a' to Add Tasks, 'r' to Remove Tasks, 's' to Show Tasks, or 'q' to Quit")
             prompt = input()
-            valid_menu_option = self.is_valid_menu_option(prompt)       
+            valid_menu_option = self.is_valid_menu_option(prompt) 
+
+        return prompt.lower() # will only return a, s, r, or q      
 
     def is_valid_menu_option(self, input):
         """
@@ -45,7 +65,7 @@ class TaskManager:
             return False
 
 
-    def add_new_task(self):
+    def add_tasks(self):
         """
             The code below executes functions for if the 'a' button is pressed.
 
@@ -74,19 +94,26 @@ class TaskManager:
                 # Add task to tasks list and increment task amount
                 self.tasks.append(task)
                 self.task_amount += 1
-    
-    def task_view(self):
-        """Displays tasks with option to return to menu"""
+
+    def remove_tasks(self):
+        """All functions to remove any one or all tasks."""
 
         self.format_tasks()
-        back_option = input("Type 'b' to go back ")
 
-        if back_option.lower() == 'b':
-            print("Heading back...")
-            self.clear_system()
+        if self.task_amount <= 0:
+            print("Nothing to remove!\n")
+                
+            self.go_back()
+    
+    def task_view(self):
+        """Displays tasks with option to return to menu."""
+
+        self.format_tasks()
+        self.go_back()
 
     def clear_system(self):
         """Handles function to clean console."""           
+        
         time.sleep(2)
         os.system('clear')
 
@@ -98,6 +125,15 @@ class TaskManager:
         if self.task_amount > 0: # If user has any tasks to display
             print("Task No.\t\tTask Name")
             for idx, task in enumerate(self.tasks, start=1):
-                print(f"{idx}\t\t\t~ {task.title()}")      
+                print(f"{idx}\t\t\t~ {task.title()}")
+    
+    def go_back(self):
+        """Handles all go back options so I didn't have to keep typing it."""
+        
+        back_option = input("Type 'b' to go back ")
+
+        if back_option.lower() == 'b':
+            print("Heading back...")
+            self.clear_system()
         
 run()
